693598e96268af1a905c858c0566fc2aef19c87c
nifma <nereszin4@gmail.com>
2025-09-01 10:22:46 +0300

    fix: add __dirname declaration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import { join, resolve } from 'path';
+import { dirname, join } from 'path';
 
 const app = express();
 const PORT = Number(process.env.PORT || 3000);
+const __dirname = dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
