7bf4b84b570737545577e338c8a6e337370a6f8f
fil_in <mihail0silaev@yandex.ru>
2025-10-25 17:05:00 +0300

    feat: add @ in path and install zustand

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,26 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // 👈 добавляем для работы alias
 
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
     server: {
         port: 5173,
         strictPort: true,
         proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true
-            }
-        }
+            "/api": {
+                target: "http://localhost:3000",
+                changeOrigin: true,
+            },
+        },
     },
     build: {
-        outDir: 'dist',
-        emptyOutDir: true
-    }
+        outDir: "dist",
+        emptyOutDir: true,
+    },
 });
