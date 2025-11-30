033594bb15e18d789fd26654c17377e9f594eaeb
nifma <100411846+nifma@users.noreply.github.com>
2025-11-30 19:29:02 +0300

    fix: wrong telegram auth logic

--- frontend/src/components/services/TelegramLoginButton.tsx
+++ frontend/src/components/services/TelegramLoginButton.tsx
@@ -8,22 +8,22 @@ export function TelegramLoginButton() {
     const containerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        (window as any).onTelegramAuth = async (user: any) => {
-            const res = await apiPost("auth/tg-login", user);
-            await loginUser(res.token);
+        (window as any).TelegramLoginWidget = {
+            dataOnauth: async (user: any) => {
+                const res = await apiPost("auth/tg-login", user);
+                await loginUser(res.token);
+            }
         };
 
         const script = document.createElement("script");
         script.src = "https://telegram.org/js/telegram-widget.js?22";
         script.setAttribute("data-telegram-login", "kpitournir_bot");
-        script.setAttribute("data-onauth", "onTelegramAuth");
+        script.setAttribute("data-onauth", "TelegramLoginWidget.dataOnauth");
         script.setAttribute("data-size", "large");
         script.async = true;
 
         containerRef.current?.appendChild(script);
     }, []);
 
-    return (
-        <div ref={containerRef} className="flex justify-center mt-4" />
-    );
+    return <div ref={containerRef} className="flex justify-center mt-4" />;
 }
