9437121b7dc689d8a3f64a660687cd7c9e293c27
nifma <nifma@vk.com>
2026-03-26 23:47:16 +0300

    fix: update teams.documents template

--- backend/src/db/migrations/010_team_documents.sql
+++ backend/src/db/migrations/010_team_documents.sql
@@ -2,9 +2,10 @@ ALTER TABLE `teams`
     ADD COLUMN `documents` TEXT NULL AFTER `special_nominations`;
 
 UPDATE `teams`
-SET `documents` = 'Согласие ОПД: 0 из 4
+SET `documents` = 'Согласие ОПД и фото-видео: 0 из 4
+Согласие на мед. вмешательство: 0 из 4
+Справка из школы: 0 из 4
 Справка об эпид. окружении: 0 из 4
-Справки из школы: 0 из 4
 
 Примечание: '
 WHERE `documents` IS NULL;
